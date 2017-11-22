c5d057d40aa903c9925618784539645481f90fdd
charles <charles@bqteam.com>
2017-11-22 11:10:02 +0800

    add

--- AppForLearn/functions/SensorVC.swift
+++ AppForLearn/functions/SensorVC.swift
@@ -14,13 +14,13 @@ class SensorVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var motionManager: CMMotionManager?
     
-    let titleArray = ["磁力计", "加速度计", "陀螺仪", "姿态", "旋转", "重力", "加速度"]
-    var valueArray = ["", "", "", "", "", "", ""]
+    let titleArray = ["磁力计", "加速度计", "陀螺仪", "姿态", "旋转", "重力", "加速度", "光线传感器", "距离传感器"]
+    var valueArray = ["", "", "", "", "", "", "", "", ""]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.navigationItem.title = "传感器的使用"
+        self.navigationItem.title = "传感器"
         tableView.delegate = self
         tableView.dataSource = self
         
@@ -30,6 +30,11 @@ class SensorVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         getAccelerationValue()
         getGyroValue()
         getDeviceMotionValue()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(getBrightness), name: .UIScreenBrightnessDidChange, object: nil)
+        
+        UIDevice.current.isProximityMonitoringEnabled = true
+        NotificationCenter.default.addObserver(self, selector: #selector(getProximityState), name: .UIDeviceProximityStateDidChange, object: nil)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -38,6 +43,9 @@ class SensorVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         motionManager?.stopGyroUpdates()
         motionManager?.stopDeviceMotionUpdates()
         motionManager = nil
+        
+        NotificationCenter.default.removeObserver(self, name: .UIScreenBrightnessDidChange, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIDeviceProximityStateDidChange, object: nil)
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -61,7 +69,7 @@ class SensorVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func getMagnetometerValue() {
         if !(motionManager?.isMagnetometerAvailable)! {
             valueArray[0] = "磁力计不可用"
-            self.tableView.reloadRows(at:[IndexPath.init(row: 0, section: 0)], with: .automatic)
+            self.tableView.reloadRows(at:[IndexPath.init(row: 0, section: 0)], with: .none)
             return
         }
         
@@ -77,7 +85,7 @@ class SensorVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             let z = Int((field?.z)!)
             let value = "x:" + x.description + ", y:" + y.description + ", z:" + z.description
             self.valueArray[0] = value
-            self.tableView.reloadRows(at:[IndexPath.init(row: 0, section: 0)], with: .automatic)
+            self.tableView.reloadRows(at:[IndexPath.init(row: 0, section: 0)], with: .none)
         })
     }
     
@@ -86,7 +94,7 @@ class SensorVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func getAccelerationValue() {
         if !((motionManager?.isAccelerometerAvailable)!) {
             valueArray[1] = "加速度计不可用"
-            self.tableView.reloadRows(at:[IndexPath.init(row: 1, section: 0)], with: .automatic)
+            self.tableView.reloadRows(at:[IndexPath.init(row: 1, section: 0)], with: .none)
             return
         }
         
@@ -99,9 +107,8 @@ class SensorVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             let x = (acceleration?.x)!
             let y = (acceleration?.y)!
             let z = (acceleration?.z)!
-            let value = "x:" + x.description + ", y:" + y.description + ", z:" + z.description
-            self.valueArray[1] = value
-            self.tableView.reloadRows(at:[IndexPath.init(row: 1, section: 0)], with: .automatic)
+            self.valueArray[1] = self.toString(x, y, z)
+            self.tableView.reloadRows(at:[IndexPath.init(row: 1, section: 0)], with: .none)
         })
     }
     
@@ -110,7 +117,7 @@ class SensorVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func getGyroValue() {
         if !((motionManager?.isGyroAvailable)!) {
             valueArray[2] = "陀螺仪不可用"
-            self.tableView.reloadRows(at:[IndexPath.init(row: 2, section: 0)], with: .automatic)
+            self.tableView.reloadRows(at:[IndexPath.init(row: 2, section: 0)], with: .none)
             return
         }
         
@@ -123,16 +130,15 @@ class SensorVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             let x = (rotationRate?.x)!
             let y = (rotationRate?.y)!
             let z = (rotationRate?.z)!
-            let value = "x:" + x.description + ", y:" + y.description + ", z:" + z.description
-            self.valueArray[2] = value
-            self.tableView.reloadRows(at:[IndexPath.init(row: 2, section: 0)], with: .automatic)
+            self.valueArray[2] = self.toString(x, y, z)
+            self.tableView.reloadRows(at:[IndexPath.init(row: 2, section: 0)], with: .none)
         })
     }
     
     func getDeviceMotionValue() {
         if !((motionManager?.isDeviceMotionAvailable)!) {
             valueArray[3] = "陀螺仪不可用"
-            self.tableView.reloadRows(at:[IndexPath.init(row: 3, section: 0)], with: .automatic)
+            self.tableView.reloadRows(at:[IndexPath.init(row: 3, section: 0)], with: .none)
             return
         }
         
@@ -147,34 +153,54 @@ class SensorVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             let x1 = (attitude?.pitch)!
             let y1 = (attitude?.roll)!
             let z1 = (attitude?.yaw)!
-            let value1 = "x:" + x1.description + ", y:" + y1.description + ", z:" + z1.description
-            self.valueArray[3] = value1
+            self.valueArray[3] = self.toString(x1, y1, z1)
             
-            // 选转
+            // 旋转
             let rotationRate = deviceMotion?.rotationRate
             let x2 = (rotationRate?.x)!
             let y2 = (rotationRate?.y)!
             let z2 = (rotationRate?.z)!
-            let value2 = "x:" + x2.description + ", y:" + y2.description + ", z:" + z2.description
-            self.valueArray[4] = value2
+            self.valueArray[4] = self.toString(x2, y2, z2)
             
             // 重力加速度
             let gravity = deviceMotion?.gravity
             let x3 = (gravity?.x)!
             let y3 = (gravity?.y)!
             let z3 = (gravity?.z)!
-            let value3 = "x:" + x3.description + ", y:" + y3.description + ", z:" + z3.description
-            self.valueArray[5] = value3
+            self.valueArray[5] = self.toString(x3, y3, z3)
             
             // 加速度
             let acceleration = deviceMotion?.userAcceleration
             let x4 = (acceleration?.x)!
             let y4 = (acceleration?.y)!
             let z4 = (acceleration?.z)!
-            let value4 = "x:" + x4.description + ", y:" + y4.description + ", z:" + z4.description
-            self.valueArray[6] = value4
+            self.valueArray[6] = self.toString(x4, y4, z4)
             
-            self.tableView.reloadRows(at:[IndexPath.init(row: 3, section: 0), IndexPath.init(row: 4, section: 0), IndexPath.init(row: 5, section: 0), IndexPath.init(row: 6, section: 0)], with: .automatic)
+            self.tableView.reloadRows(at:[IndexPath.init(row: 3, section: 0), IndexPath.init(row: 4, section: 0), IndexPath.init(row: 5, section: 0), IndexPath.init(row: 6, section: 0)], with: .none)
         })
+    }
+    
+    // MARK: - 光线传感器
+    
+    @objc func getBrightness() {
+        let brightness = UIScreen.main.brightness
+        self.valueArray[7] = self.toString(Double(brightness))
+        self.tableView.reloadRows(at:[IndexPath.init(row: 7, section: 0)], with: .none)
+    }
+    
+    @objc func getProximityState() {
+        let proximityState = UIDevice.current.proximityState
+        self.valueArray[8] = proximityState ? "有物体接近" : "有物体远离"
+        self.tableView.reloadRows(at:[IndexPath.init(row: 8, section: 0)], with: .none)
+    }
+    
+    func toString(_ value: Double) -> String {
+        let i = Int(value * 10000)
+        let d = Double(i) / 10000
+        return d.description
+    }
+    
+    func toString(_ value1: Double, _ value2: Double, _ value3: Double) -> String {
+        return "x:" + self.toString(value1) + ", y:" + self.toString(value2) + ", z:" + self.toString(value3)
     }
 }
